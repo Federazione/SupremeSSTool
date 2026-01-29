60832116f830fde3e1a2b9748de2897d69b2c899
FederazioneDev <federazionehog@gmail.com>
2026-01-29 05:10:57 -0800

    Delete core directory

--- core/helpers.ps1
+++
@@ -1,8 +0,0 @@
-$global:Timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$global:Evidence = "$env:USERPROFILE\Desktop\FED_EVIDENCE_$Timestamp"
-New-Item $Evidence -ItemType Directory -Force | Out-Null
-
-function Save($name, $content) {
-    $path = Join-Path $Evidence $name
-    $content | Out-File $path -Encoding UTF8
-}
--- core/system.ps1
+++
@@ -1,8 +0,0 @@
-$info = @(
-    "User: $env:USERNAME"
-    "OS: $((Get-CimInstance Win32_OperatingSystem).Caption)"
-    "Build: $((Get-CimInstance Win32_OperatingSystem).BuildNumber)"
-)
-
-$info | ForEach-Object { Log $_ }
-Save "system.txt" $info
